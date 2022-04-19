d381b8d9469f6aef55b364c4ae5d0d01c6d13d51
Woojoo Oh <space8237@naver.com>
2022-04-19 17:24:51 +0900

    419edition2

--- ac4_16.java
+++ ac4_16.java
@@ -1,10 +1,60 @@
 package bj_problem_ac;
 
+import java.util.Scanner;
+
 public class ac4_16 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		sc.nextLine();
+		int ans = 0;
+		
+		if(N>=M) {
+			int[] a = new int[N];
+			int[] b = new int[N];
 
+			for(int i=0; i<N; i++) {
+				a[i] = sc.nextInt();
+			}
+			sc.nextLine();
+			
+			for(int i=0; i<M; i++) {
+				b[i] = sc.nextInt();
+			}
+			for(int i=M; i<N; i++) {
+				b[i] = 0;
+			}
+			
+			for(int i=0; i<N; i++) {
+				ans += a[i]-b[i];
+			}
+			System.out.println(ans);
+			
+		}else {
+			int[] a = new int[M];
+			int[] b = new int[M];
+
+			for(int i=0; i<N; i++) {
+				a[i] = sc.nextInt();
+			}
+			for(int i=N; i<M; i++) {
+				a[i] = 0;
+			}
+			sc.nextLine();
+			for(int i=0; i<M; i++) {
+				b[i] = sc.nextInt();
+			}
+			for(int i=0; i<M; i++) {
+				ans += a[i]-b[i];
+			}
+			System.out.println(ans);
+		}
+		
+		sc.close();
+		
 	}
 
 }
